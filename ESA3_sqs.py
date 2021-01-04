b2a6a6b0f3719680bcac194bcf03ef0827ec80bc
Chris <70528080+sirhC-B@users.noreply.github.com>
2021-01-04 19:28:57 +0100

    Add files via upload

---
+++ ESA3_sqs.py
@@ -0,0 +1,25 @@
+import boto3
+
+session = boto3.session.Session()
+
+sqs = session.client('sqs', region_name='us-east-1')
+
+r = sqs.send_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/790933937313/xmas2020',
+    MessageAttributes={
+    'MatNr':{ 'DataType': 'Number',
+            'StringValue': '20211774',
+            },
+    'StudentName':{ 'DataType': 'String',
+                    'StringValue': 'Chris Boesener',
+            },
+    'Email-Adresse':{ 'DataType': 'String',
+                      'StringValue': 'boesener@th-brandenburg.de',
+            },
+    'ReplyUrl':{ 'DataType': 'String',
+                 'StringValue': 'https://sqs.us-east-1.amazonaws.com/272763356340/testQueue',
+            },},
+    MessageBody=('ESA3 - SQS \n\n MatNr: 20211774 \n StudentName: Chris Boesener \n Email-Adresse: boesener@th-brandenburg.de \n ReplyUrl: https://sqs.us-east-1.amazonaws.com/272763356340/testQueue \n\n Gesundes neues Jahr! ')
+    )
+
+print (r)
